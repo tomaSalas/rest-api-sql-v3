2dcb71907b6cf93132e47b6d449e83491b81c16a
tomaSalas <tomassubero97@gmail.com>
2021-06-07 13:58:25 -0500

    added implicit field to course model

--- models/course.js
+++ models/course.js
@@ -3,6 +3,11 @@ const { Model, DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
     class Course extends Model {}
     Course.init({
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
       title: {
         type: DataTypes.STRING,
         allowNull: false,
